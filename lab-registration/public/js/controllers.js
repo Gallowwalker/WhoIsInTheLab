b9e861d3e7e8044d48f679485bce4f86c5aebe67
Rangel Ivanov <rangelivanov88@gmail.com>
2014-06-08 15:50:43 +0300

    Initial implementation of add device form

--- lab-registration/public/js/controllers.js
+++ lab-registration/public/js/controllers.js
@@ -1,8 +1,23 @@
-var labRegistration = angular.module('lab-registration', []);
+var labRegistration = angular.module('lab-registration', ['ngSanitize', 'ui.select']);
+
 
 labRegistration.controller('LabRegCtrl', ['$scope', '$http', function ($scope, $http) {
 
-	$http.get('/users').success(function(data) {
-		$scope.users = data;
+	$scope.device = {};
+	$scope.user = {};
+	$scope.user.selected = undefined;
+
+	$http.get('/users').success(function(users) {
+		$scope.users = users;
 	});
+
+	$http.get('/mac').success(function(data) {
+		$scope.device.mac = data.mac;
+	}).error(function(error) {
+		$scope.device.mac = error.message;
+	});
+
+	$scope.submitDevice = function(device) {
+		// TODO: call add device api
+	};
 }]);
